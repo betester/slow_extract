fbf13f3f59ecc76af9723bd8efc5610c67040a01
betester <ikramzulkarnaen02@gmail.com>
2023-10-03 23:08:36 +0700

    add tqdm to show index progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/slow_extract
 
 go 1.20
+
+require github.com/sbwhitecap/tqdm v0.0.0-20170314014342-7929e3102f57 // indirect
--- isorter/bsbi.go
+++ isorter/bsbi.go
@@ -8,6 +8,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/sbwhitecap/tqdm"
+	. "github.com/sbwhitecap/tqdm/iterators"
 	"github.com/slow_extract/index"
 	"github.com/slow_extract/mapper"
 	"github.com/slow_extract/utils"
@@ -33,12 +35,15 @@ func (bsbi *Bsbi) CreateCollectionIndex(collectionPath string) error {
 	}
 
 	invertedIndexHeap := index.InitHeap()
+	var iterationError error = nil
 
-	for _, blockPath := range blockPaths {
-
+	tqdm.With(Strings(blockPaths), "Iterating Collections", func(v interface{}) (brk bool) {
+		blockPath := v.(string)
 		invertedIndex, err := bsbi.parseBlock(collectionPath, blockPath)
+
 		if err != nil {
-			return err
+			iterationError = err
+			return true
 		}
 		indexName := fmt.Sprintf("i-%s", blockPath)
 		indexWriter := index.InvertedIndex{}
@@ -46,8 +51,14 @@ func (bsbi *Bsbi) CreateCollectionIndex(collectionPath string) error {
 		invertedIndexHeap.Push(indexWriter.Iterator())
 
 		if err := indexWriter.Write(invertedIndex); err != nil {
-			return err
+			iterationError = err
+			return true
 		}
+		return
+	})
+
+	if iterationError != nil {
+		return iterationError
 	}
 
 	return bsbi.mergeIndices(invertedIndexHeap)
@@ -86,8 +97,9 @@ func (bsbi *Bsbi) mergeIndices(invertedIndexHeap heap.Interface) error {
 
 		heap.Push(invertedIndexHeap, smallestIterator)
 	}
-
 }
+
+
 func (bsbi *Bsbi) parseBlock(collectionPath, blockPath string) (map[uint32][]uint32, error) {
 	fullBlockPath := fmt.Sprintf("%s/%s", collectionPath, blockPath)
 	blockFolder, err := os.Open(fullBlockPath)
@@ -104,12 +116,17 @@ func (bsbi *Bsbi) parseBlock(collectionPath, blockPath string) (map[uint32][]uin
 		return nil, err
 	}
 
-	for _, filePath := range filePaths {
+	var iterationErr error = nil
+
+	tqdm.With(Strings(filePaths), "Iterating text files", func(v interface{}) (brk bool) {
+
+		filePath := v.(string)
 		fullFilePath := fmt.Sprintf("%s/%s", fullBlockPath, filePath)
 		file, err := os.OpenFile(fullFilePath, os.O_RDONLY, 0755)
 
 		if err != nil {
-			return nil, err
+			iterationErr = err
+			return true
 		}
 
 		buffer := bufio.NewScanner(file)
@@ -125,18 +142,24 @@ func (bsbi *Bsbi) parseBlock(collectionPath, blockPath string) (map[uint32][]uin
 				tdPairs = append(tdPairs, tdPair)
 			}
 		}
-	}
 
-	sort.Slice(tdPairs, func(i, j int) bool {
-		return tdPairs[i][1] <= tdPairs[j][1]
-	})
+		sort.Slice(tdPairs, func(i, j int) bool {
+			return tdPairs[i][1] <= tdPairs[j][1]
+		})
 
-	for _, tdPair := range tdPairs {
-		if _, ok := invertedIndex[tdPair[0]]; !ok {
-			invertedIndex[tdPair[0]] = make([]uint32, 0)
+		for _, tdPair := range tdPairs {
+			if _, ok := invertedIndex[tdPair[0]]; !ok {
+				invertedIndex[tdPair[0]] = make([]uint32, 0)
+			}
+
+			invertedIndex[tdPair[0]] = append(invertedIndex[tdPair[0]], tdPair[1])
 		}
 
-		invertedIndex[tdPair[0]] = append(invertedIndex[tdPair[0]], tdPair[1])
+		return
+	})
+
+	if iterationErr != nil {
+		return nil, iterationErr
 	}
 
 	return invertedIndex, nil
